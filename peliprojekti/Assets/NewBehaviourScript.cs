048f3e8b37403b3e05b27c30e0a7b3cafae779d7
Valtteri Kähärä <valtterikahara@gmail.com>
2019-01-23 09:31:10 +0200

    Liikkuvuus
    toimii toivottavasti
    
    -seuraavaksi joku tekee tasosuunnittelutyökalut

--- peliprojekti/Assets/NewBehaviourScript.cs
+++ peliprojekti/Assets/NewBehaviourScript.cs
@@ -7,12 +7,19 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.transform.Translate(Input.GetAxis("Horizontal"), 0, 0);
+
     }
+
+
+
+
 }
+
+
