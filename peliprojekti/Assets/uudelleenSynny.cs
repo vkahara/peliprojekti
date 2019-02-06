803e16a44ef2383de2b506f415f5d1c8cf0b73e5
Valtteri Kähärä <valtterikahara@gmail.com>
2019-02-06 22:50:59 +0200

    DeathMenu scene, uudelleenSynny.cs -tiedostoon lisätty kuolemia laskeva int

--- peliprojekti/Assets/uudelleenSynny.cs
+++ peliprojekti/Assets/uudelleenSynny.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class uudelleenSynny : MonoBehaviour
 {
     float fallZone = -10f;
     public Transform playerSpawnPoint = null;
+    
+    int kuolemaLaskuri = 0;
+
     void Update()
     {
         if (transform.position.y < fallZone)
         {
             transform.position = playerSpawnPoint.position;
+            kuolemaLaskuri++;
+        }
+
+        if (kuolemaLaskuri >= 3)
+        {
+            SceneManager.LoadScene(2);
         }
     }
 }
